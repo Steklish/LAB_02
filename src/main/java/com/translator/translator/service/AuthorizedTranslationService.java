6b9c10159c18aa4b54c6d646f4445d6444aa7d35
Steklish <anton.kozlov.mailbox@gmail.com>
2025-03-18 09:49:26 +0300

    fianlly working

--- src/main/java/com/translator/translator/service/AuthorizedTranslationService.java
+++ src/main/java/com/translator/translator/service/AuthorizedTranslationService.java
@@ -1,6 +1,7 @@
 package com.translator.translator.service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,18 +35,14 @@ public class AuthorizedTranslationService {
 
     public List<String> verifyTranslate( String srcLan, 
             String destLang,  String text, long id) throws JsonProcessingException {
-        Optional<User> userOptional = Optional.ofNullable(userService.getUserById(id));
-
-        // Check if the user exists
-        if (userOptional.isEmpty()) {
-            // throw new NoSuchElementException("No user found with ID: " + id);
-            return List.of("error", "NO USER FOUND");
-        }
-        else {
+        try {
+            Optional<User> userOptional = Optional.ofNullable(userService.getUserById(id));
             List<String> result = resolveQueryService.getTranslation(srcLan, destLang, text);
             Translation newTranslation = new Translation(srcLan, destLang, text, result.get(0));
             translationService.createTranslation(id, newTranslation);
             return result;
+        } catch (NoSuchElementException e) {
+            return List.of("error", "NO USER FOUND");
         }
     }
 }
